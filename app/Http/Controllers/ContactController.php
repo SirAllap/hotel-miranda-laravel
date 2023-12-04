c179b97abf9ea25c5cd147aef8c943ddbafc8d7e
David PR <serallapdivad@gmail.com>
2023-12-04 20:36:07 +0000

    created the controller for contact

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function contact(Request $request)
+    {
+        if ($request->isMethod('post')) {
+            $request->validate([
+                'name' => 'required|string',
+                'email' => 'required|string|email',
+                'phone' => 'required|string',
+                'subject' => 'required|string',
+                'message' => 'required|string',
+            ]);
+
+            $full_name = $request->input('name');
+            $email = $request->input('email');
+            $phone_number = $request->input('phone');
+            $subject_of_review = $request->input('subject');
+            $review_body = $request->input('message');
+
+            Contact::create([
+                'full_name' => $full_name,
+                'email' => $email,
+                'phone_number' => $phone_number,
+                'subject_of_review' => $subject_of_review,
+                'review_body' => $review_body,
+            ]);
+
+            $confirmation = 'Thank you! Your review has been sent to our team.';
+            $error = false;
+            return view('contact', ['confirmation' => $confirmation, 'error' => $error]);
+        } else {
+            return view('contact');
+        }
+    }
+}
