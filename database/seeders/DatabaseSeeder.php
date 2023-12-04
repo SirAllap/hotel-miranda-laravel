762e5ba594a91e16b6337de873916efbb15b7268
David PR <serallapdivad@gmail.com>
2023-12-04 15:03:23 +0000

    fixed db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,10 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        \DB::unprepared(file_get_contents('./seed.sql'));
+        DB::unprepared(file_get_contents('./seed.sql'));
     }
 }
