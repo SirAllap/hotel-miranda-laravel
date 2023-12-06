a3bfc1405109d32a74b674a43efa23510e6f4918
David PR <serallapdivad@gmail.com>
2023-12-06 10:08:25 +0000

    Added the relation with the bookings on rooms model

--- app/Models/Rooms.php
+++ app/Models/Rooms.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Rooms extends Model
 {
     use HasFactory;
     protected $table = 'room';
+
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
