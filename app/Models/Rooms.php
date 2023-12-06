3963919f6fce42f06e3d2d8d955a20087f57536c
David PR <serallapdivad@gmail.com>
2023-12-06 12:34:24 +0000

    Rooms Model: generated a new static method to check single room availability

--- app/Models/Rooms.php
+++ app/Models/Rooms.php
@@ -39,4 +39,29 @@ class Rooms extends Model
             )
             ->get();
     }
+
+    public static function single_room_availability($id, $check_in, $check_out)
+    {
+        return Rooms
+            ::select('room.*', 'photo.URL', DB::raw("GROUP_CONCAT(a.amenities SEPARATOR ', ') AS all_amenities"))
+            ->join('photo', 'room.id', '=', 'photo.room_id')
+            ->join('amenities_has_room as ahr', 'room.id', '=', 'ahr.room_id')
+            ->join('amenity as a', 'ahr.amenity_id', '=', 'a.id')
+            ->where('room.id', '=', $id)
+            ->whereNotExists(
+                function ($query) use ($check_in, $check_out) {
+                    $query->select(DB::raw(1))
+                        ->from('booking as b')
+                        ->whereColumn('room.id', '=', 'b.room_id')
+                        ->where(
+                            function ($subquery) use ($check_in, $check_out) {
+                                $subquery->whereBetween('b.check_in', [$check_in, $check_out])
+                                    ->orWhereBetween('b.check_out', [$check_in, $check_out]);
+                            }
+                        );
+                }
+            )
+            ->groupBy('room.id', 'photo.URL')
+            ->first();
+    }
 }
