ec98ddd0f6bffee17171f951bd7ee0d67ee3ec79
David PR <serallapdivad@gmail.com>
2023-12-06 12:35:29 +0000

    RoomDetailsController: Removed the code to check availability, now we check it by a static method on the model

--- app/Http/Controllers/RoomDetailsController.php
+++ app/Http/Controllers/RoomDetailsController.php
@@ -51,34 +51,13 @@ class RoomDetailsController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Request $request)
+    public function show($id, Request $request)
     {
-        if ($request->input('trip-start') && $request->input('trip-end')) {
-            $id = $request->input('room_id');
+        if ($request->input('trip-start') && $request->input('trip-end')) {;
             $trip_start = $request->input('trip-start');
             $trip_end = $request->input('trip-end');
 
-            $room = Rooms
-                ::select('room.*', 'photo.URL', DB::raw("GROUP_CONCAT(a.amenities SEPARATOR ', ') AS all_amenities"))
-                ->join('photo', 'room.id', '=', 'photo.room_id')
-                ->join('amenities_has_room as ahr', 'room.id', '=', 'ahr.room_id')
-                ->join('amenity as a', 'ahr.amenity_id', '=', 'a.id')
-                ->where('room.id', '=', $id)
-                ->whereNotExists(
-                    function ($query) use ($trip_start, $trip_end) {
-                        $query->select(DB::raw(1))
-                            ->from('booking as b')
-                            ->whereColumn('room.id', '=', 'b.room_id')
-                            ->where(
-                                function ($subquery) use ($trip_start, $trip_end) {
-                                    $subquery->whereBetween('b.check_in', [$trip_start, $trip_end])
-                                        ->orWhereBetween('b.check_out', [$trip_start, $trip_end]);
-                                }
-                            );
-                    }
-                )
-                ->groupBy('room.id', 'photo.URL')
-                ->first();
+            $room = Rooms::single_room_availability($id, $trip_start, $trip_end);
 
             $rooms = Rooms
                 ::join('photo', 'room.id', '=', 'photo.room_id')
@@ -94,7 +73,6 @@ class RoomDetailsController extends Controller
             }
             return view('room-details', ['room' => $room, 'rooms' => $rooms, 'start' => $trip_start, 'end' => $trip_end]);
         } else {
-            $id = $request->input('room_id');
             $_SESSION['id'] = $id;
             isset($_SESSION['start']) ? $trip_start = $_SESSION['start'] : $trip_start = null;
             isset($_SESSION['end']) ? $trip_end = $_SESSION['end'] : $trip_end = null;
