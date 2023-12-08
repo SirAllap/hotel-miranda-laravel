5b3f14faee6489e61228a6de118e71aa9b8e2273
David PR <serallapdivad@gmail.com>
2023-12-08 12:55:29 +0000

    Simplify how we sent the data from the html form

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -15,31 +15,16 @@ class BookingController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|string',
-            'phone' => 'required|string',
+            'guest' => 'required|string',
+            'phone_number' => 'required|string',
             'email' => 'required|string|email',
-            'trip-start' => 'required|string',
-            'trip-end' => 'required|string',
-            'special-request' => 'required|string',
+            'check_in' => 'required|string',
+            'check_out' => 'required|string',
+            'special_request' => 'required|string',
+            'room_id' => 'required|integer',
         ]);
 
-        $guest_name = $request->input('name');
-        $phone_number = $request->input('phone');
-        $email = $request->input('email');
-        $check_in = $request->input('trip-start');
-        $check_out = $request->input('trip-end');
-        $special_request = $request->input('special-request');
-        $id = $_SESSION['id'];
-
-        Booking::create([
-            'guest' => $guest_name,
-            'phone_number' => $phone_number,
-            'email' => $email,
-            'check_in' => $check_in,
-            'check_out' => $check_out,
-            'special_request' => $special_request,
-            'room_id' => $id,
-        ]);
+        Booking::created($request->all());
 
         $confirmation = 'Thank you for your request. We have received it correctly. Someone from our Team will get back to you very soon.';
         $error = false;
