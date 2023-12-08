a6d3fc580f5ae7338e8e7c4f02c6d067edb9fac1
David PR <serallapdivad@gmail.com>
2023-12-08 12:56:08 +0000

    separate the logic into two methods

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,24 +7,24 @@ use App\Models\Contact;
 
 class ContactController extends Controller
 {
-    public function contact(Request $request)
+    public function show(Request $request)
     {
-        if ($request->isMethod('post')) {
-            $request->validate([
-                'full_name' => 'required|string',
-                'email' => 'required|string|email',
-                'phone_number' => 'required|string',
-                'subject_of_review' => 'required|string',
-                'review_body' => 'required|string',
-            ]);
+        return view('contact');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'full_name' => 'required|string',
+            'email' => 'required|string|email',
+            'phone_number' => 'required|string',
+            'subject_of_review' => 'required|string',
+            'review_body' => 'required|string',
+        ]);
 
-            Contact::create($request->all());
+        Contact::create($request->all());
 
-            $confirmation = ['Thank you! Your review has been sent to our team.'];
-            $error = false;
-            return view('contact')->with(['confirmation' => $confirmation, 'error' => $error]);
-        } else {
-            return view('contact');
-        }
+        $confirmation = ['Thank you! Your review has been sent to our team.'];
+        $error = false;
+        return view('contact')->with(['confirmation' => $confirmation, 'error' => $error]);
     }
 }
