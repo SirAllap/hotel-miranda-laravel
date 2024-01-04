49ed8f2f94cd1ab76ed36dafe826fb4643a8b17b
David PR <serallapdivad@gmail.com>
2024-01-04 17:31:20 +0000

    updated favicon path

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="{{ URL::asset('css/styles/style.css') }}" />
-    <link rel="shortcut icon" href="../../public/favicon.ico" type="image/ico" />
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/ico" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
     <title>@yield('title')</title>
